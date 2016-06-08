a8bbff4c2d5832aa604e26cb05f94df38ff11290
Niels Snakenborg <nc.snakenborg@student.avans.nl>
2016-06-08 16:16:54 +0200

    Tiles with images

--- app/js/directives/tileDirective.js
+++ app/js/directives/tileDirective.js
@@ -1,4 +1,4 @@
-module.exports = function(){
+module.exports = function($filter){
 
     return {
         restrict: 'AE',
@@ -11,12 +11,14 @@ module.exports = function(){
         templateUrl: "/js/templates/tile.html",
         controller: function($scope, $element){
 
-
         },
         link: function(scope, element, attributes){
-            element.css('left', '' + scope.tileModel.xPos * 27);
-            element.css('top', '' + scope.tileModel.yPos * 27);
+            element.css('left', '' + scope.tileModel.xPos * 20);
+            element.css('top', '' + scope.tileModel.yPos * 34.5);
             element.css('z-index', '' + scope.tileModel.zPos);
+
+            var myFilter = $filter('tileFilter');
+            element.css('background-position-y', '' + myFilter(scope.tileModel.tile.suit, scope.tileModel.tile.name));
         }
     };
-};
+}
--- app/js/filters/tileFilter.js
+++ app/js/filters/tileFilter.js
@@ -8,120 +8,36 @@ module.exports = function () {
     switch((tile + " " + name)){
         // Bamboo
         case "Bamboo 1":
-        output = "z"
+        output = "0px"
         break;
         case "Bamboo 2":
-        output = "x"
+        output = "-69px"
         break;
         case "Bamboo 3":
-        output = "c"
+        output = "-138px"
         break;
         case "Bamboo 4":
-        output = "v"
+        output = "-206px"
         break;
         case "Bamboo 5":
-        output = "b"
+        output = "-275px"
         break;
         case "Bamboo 6":
-        output = "n"
+        output = "-1994px"
         break;
         case "Bamboo 7":
-        output = "m"
+        output = "-1994px"
         break;
         case "Bamboo 8":
-        output = ","
+        output = "-1994px"
         break;
         case "Bamboo 9":
-        output = "."
+        output = "-1994px"
         break;
 
-        // Characters
-        case "Character 1":
-        output = "a"
-        break;
-        case "Character 2":
-        output = "s"
-        break;
-        case "Character 3":
-        output = "d"
-        break;
-        case "Character 4":
-        output = "f"
-        break;
-        case "Character 5":
-        output = "g"
-        break;
-        case "Character 6":
-        output = "h"
-        break;
-        case "Character 7":
-        output = "j"
-        break;
-        case "Character 8":
-        output = "k"
-        break;
-        case "Character 9":
-        output = "l"
-        break;
-
-        // Circle
-        case "Circle 1":
-        output = "q"
-        break;
-        case "Circle 2":
-        output = "w"
-        break;
-        case "Circle 3":
-        output = "e"
-        break;
-        case "Circle 4":
-        output = "r"
-        break;
-        case "Circle 5":
-        output = "t"
-        break;
-        case "Circle 6":
-        output = "y"
-        break;
-        case "Circle 7":
-        output = "u"
-        break;
-        case "Circle 8":
-        output = "i"
-        break;
-        case "Circle 9":
-        output = "o"
-        break;
-
-        // Flowers
-        case "Flower Orchid":
-        output = "!"
-        break;
-        case "Flower Plum":
-        output = '"';
-        break;
-        case "Flower Bamboo":
-        output = "#"
-        break;
-        case "Flower Chrysantememum":
-        output = "€"
-        break;
-
-        // Wind
-        case "Wind North":
-        output = "1"
-        break;
-        case "Wind East":
-        output = "2"
-        break;
-        case "Wind South":
-        output = "3"
-        break;
-        case "Wind West":
-        output = "4"
-        break;
-
-        default: console.log(tile + " " + name);
+        default:
+        output = "-1994px"
+        console.log(tile + " " + name);
     }
 
     return output;
